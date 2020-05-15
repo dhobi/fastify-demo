b7dcfc24c6169022c03af94231c35d5b7ded9f21
Daniel Hobi <info@danielhobi.ch>
2020-05-15 11:06:12 +0200

    Fix: registering plugins is async.

--- main.ts
+++ main.ts
@@ -3,28 +3,32 @@ import fastifyStatic from 'fastify-static';
 import fastifyBasicAuth from 'fastify-basic-auth';
 import * as path from "path";
 
-const port = Number(process.env.PORT) || 5003;
-const server = fastify();
+async function build () {
+    const port = Number(process.env.PORT) || 5003;
+    const server = fastify();
 
-server.register(fastifyStatic, {
-    root: path.join(__dirname, 'admin', 'public'),
-    prefix: '/admin/public/',
-});
-server.register(fastifyBasicAuth, {
-    validate: (username, password, req, reply, done) => {
-        if (username === 'fastifydemo' && password === 'fastifydemo') {
-            done()
-        } else {
-            done(new Error('No access'))
+    await server.register(fastifyStatic, {
+        root: path.join(__dirname, 'admin', 'public'),
+        prefix: '/admin/public/',
+    });
+    await server.register(fastifyBasicAuth, {
+        validate: (username, password, req, reply, done) => {
+            if (username === 'fastifydemo' && password === 'fastifydemo') {
+                done()
+            } else {
+                done(new Error('No access'))
+            }
+        },
+        authenticate: true
+    });
+    server.addHook('onRequest', server.basicAuth);
+
+    server.listen(port, '0.0.0.0', (err, address) => {
+        if (err) {
+            console.error(err);
         }
-    },
-    authenticate: true
-});
-server.addHook('onRequest', server.basicAuth);
-
-server.listen(port, '0.0.0.0', (err, address) => {
-    if (err) {
-        console.error(err);
-    }
-    console.log(`Server listening at ${address}`)
-})
+        console.log(`Server listening at ${address}`)
+    });
+    return server;
+}
+build();
